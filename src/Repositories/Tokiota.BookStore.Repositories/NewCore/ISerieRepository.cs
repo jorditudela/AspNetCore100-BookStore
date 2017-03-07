41191131da25047526f6e6c6318c1d38b253dc33
Jordi Tudela <jordi.tudela@grupoinfomed.es>
2017-03-07 15:25:52 +0100

    Add SerieRepository class and Interface

---
+++ src/Repositories/Tokiota.BookStore.Repositories/NewCore/ISerieRepository.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Tokiota.BookStore.Repositories.NewCore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Tokiota.BookStore.Entities;
+    interface ISerieRepository
+    {
+        IEnumerable<Serie> GetList();
+
+        Serie GetById(Guid Id);
+
+        Guid Create(Serie serie);
+
+        bool Delete(Guid id);
+
+        bool Update(Guid id);
+    }
+}
---
+++ src/Repositories/Tokiota.BookStore.Repositories/NewCore/SerieRepository.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Tokiota.BookStore.Repositories.NewCore
+{
+    using Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class SerieRepository
+    {
+        public IEnumerable<Serie> GetList()
+        {
+            return null;
+        }
+
+        public Serie GetById(Guid Id)
+        {
+            return null;
+        }
+
+        public Guid Create(Serie serie)
+        {
+            return new Guid();
+        }
+
+        public bool Delete(Guid id)
+        {
+            return true;
+        }
+
+        public bool Update(Guid id)
+        {
+            return true;
+        }
+
+    }
+}
